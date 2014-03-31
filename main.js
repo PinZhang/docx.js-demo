b3c4648220acaabe8fd8231179c0450cec5297c1
Pin Zhang <pzhang@mozilla.com>
2014-04-01 00:01:25 +0800

    Init commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function $id(id) {
+  return document.getElementById(id);
+}
+
+function convert() {
+  var selected_file = $id('file').files[0];
+  var reader = new FileReader();
+
+  reader.onload = function(aEvent) {
+    convertToPDF(btoa(aEvent.target.result));
+  };
+
+  // reader.readAsArrayBuffer(selected_file);
+  reader.readAsBinaryString(selected_file);
+}
+
+function convertToPDF(aDocxContent) {
+  var content = docx(aDocxContent);
+  $id('container').contentText = '';
+  console.log('content length: ' + content.DOM.length);
+  while (content.DOM.length > 0) {
+    var node = content.DOM[0];
+    $id('container').appendChild(node);
+  }
+}
+
+window.addEventListener('load', function() {
+  document.getElementById('convert').onclick = convert;
+});
+
